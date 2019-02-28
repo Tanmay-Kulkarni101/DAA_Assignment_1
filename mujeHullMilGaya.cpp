25186dfa142ca9284f55fc226f6c7a64c7c6a48a
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-02-28 20:54:44 +0530

    Added the sort functionality

--- mujeHullMilGaya.cpp
+++ mujeHullMilGaya.cpp
@@ -23,9 +23,8 @@ class Data{
 		this->delim=delim;
 	}
 };
-// class Utilities{
 
-// }
+	
 class Node{
 	private:
 	double x;
@@ -35,11 +34,26 @@ class Node{
 		this->x=x;
 		this->y=y;
 	}
+	std::pair<double,double> getPoint(){
+		return std::make_pair(x,y);
+	}
+	static bool compareX(void* a,void* b){
+		Node val1=*((Node*)a);
+		Node val2=*((Node*)b);
+		if(val1.x<=val2.x){
+			return true;
+		}
+		else{
+			false;
+		}
+	}
 };
+
 class Edge{
 	private:
 	Node point1,point2;
 };
+
 class Graph{
 	private:
 	std::vector<Node> convexHull;
@@ -69,9 +83,65 @@ class Graph{
 		}
 
 	}
+
+	std::vector<Node> getPointList(){
+		return pointList;
+	}
+
 };
+
+class Utilities{
+	public:
+	Utilities(){
+
+	}
+
+	template <typename T>
+	void sort(std::vector<T> array,bool (*compare)(void* a,void* b) ){
+		std::size_t const mid=array.size()/2;
+		std::vector<T> low(array.begin(),array.begin()+mid);
+		std::vector<T> high(array.begin()+mid,array.end());
+		sort(low,compare);
+		sort(high,compare);
+
+		std::vector<T> temp;
+		typename std::vector<T>::iterator left=low.begin();
+		typename std::vector<T>::iterator right=high.begin();
+		while(left<low.end() && right<high.end()){
+			if( (*compare) ( (void*)&*left,(void*)&*right ) ) {//left smaller than right
+				temp.push_back(*left);
+			}
+			else{
+				temp.push_back(*right);
+			}
+		}
+		while(left<low.end()){
+			temp.push_back(*left);
+		}
+		while(right<high.end()){
+			temp.push_back(*right);
+		}
+
+		typename std::vector<T>::iterator iter1,iter2;
+		for(iter1=temp.begin(),iter2=array.begin();iter1<temp.end();iter1++,iter2++){
+			*iter2=*iter1;
+		}
+	}
+
+	void mom(){
+
+	}
+};
+
+
 int main(){
 	Data data=Data("./test.txt");
 	Graph graph=Graph(data);
+	Utilities utility=Utilities();
+	utility.sort(graph.getPointList(),(&Node::compareX) );
+	std::vector<Node>::iterator it= graph.getPointList().begin();
+	for(;it<graph.getPointList().end();it++){
+		std::cout<<it->getPoint().first<<" "<<it->getPoint().second<<std::endl;
+	}
 	return 0;
 }
