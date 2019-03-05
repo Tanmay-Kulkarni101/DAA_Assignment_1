2a148917115ba67e73fb127f622972b8cc6d2dd5
Vishalcross <vishalcross36@gmail.com>
2019-03-05 16:38:09 +0530

    Added the Graph header and c++ files

--- mujeHullMilGaya.cpp
+++ mujeHullMilGaya.cpp
@@ -6,57 +6,59 @@
 #include "data.h"
 #include "node.h"
 #include "utilities.h"
-// #include "edge.h"
+#include "stack.h"
+#include "edge.h"
+#include "graph.h"
 
-class Edge{
-	private:
-	Node point1,point2;
-};
+// class Edge{
+// 	private:
+// 	Node point1,point2;
+// };
 
-class Graph{
-	private:
-	std::vector<Node> convexHull;
-	std::vector<Node> pointList;
-	// std::vector<Edge> edges;
-	public:
-
-	Graph(Data data){
-
-		if(data.getFilePath().empty()==0){
-			std::fstream fileDes;
-			fileDes.open(data.getFilePath(),std::ios::in);
-			std::string line;
-			std::vector<std::string> coordinates;
-			while(fileDes.eof() == 0){
-				std::getline(fileDes,line);
-				boost::split(coordinates,line,boost::is_any_of(data.getDelim()));
-				if(coordinates.size()==2){
-					pointList.push_back( Node ( std::stod(coordinates.at(0)),std::stod(coordinates.at(1) ) ) );
-				}
-				else{
-					std::cerr<<"This file has been incorrectly formatted\n";
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-
-	}
-
-	std::vector<Node> getPointList(){
-		return pointList;
-	}
-
-	void setPointList(std::vector<Node> &pointList){
-		this->pointList.swap(pointList);
-	}
-
-	void displayPointList(){
-		for(auto it=pointList.begin();it<pointList.end();it++){
-			std::cout<<"X:"<<it->getPoint().first<<" Y:"<<it->getPoint().second<<std::endl;
-		}
-	}
-
-};
+// class Graph{
+// 	private:
+// 	std::vector<Node> convexHull;
+// 	std::vector<Node> pointList;
+// 	// std::vector<Edge> edges;
+// 	public:
+//
+// 	Graph(Data data){
+//
+// 		if(data.getFilePath().empty()==0){
+// 			std::fstream fileDes;
+// 			fileDes.open(data.getFilePath(),std::ios::in);
+// 			std::string line;
+// 			std::vector<std::string> coordinates;
+// 			while(fileDes.eof() == 0){
+// 				std::getline(fileDes,line);
+// 				boost::split(coordinates,line,boost::is_any_of(data.getDelim()));
+// 				if(coordinates.size()==2){
+// 					pointList.push_back( Node ( std::stod(coordinates.at(0)),std::stod(coordinates.at(1) ) ) );
+// 				}
+// 				else{
+// 					std::cerr<<"This file has been incorrectly formatted\n";
+// 					exit(EXIT_FAILURE);
+// 				}
+// 			}
+// 		}
+//
+// 	}
+//
+// 	std::vector<Node> getPointList(){
+// 		return pointList;
+// 	}
+//
+// 	void setPointList(std::vector<Node> &pointList){
+// 		this->pointList.swap(pointList);
+// 	}
+//
+// 	void displayPointList(){
+// 		for(auto it=pointList.begin();it<pointList.end();it++){
+// 			std::cout<<"X:"<<it->getPoint().first<<" Y:"<<it->getPoint().second<<std::endl;
+// 		}
+// 	}
+//
+// };
 
 
 
