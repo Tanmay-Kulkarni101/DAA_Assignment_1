161ab837f124b8a6b85c80e7e3b1832f769e4067
tanmay_kulkarni <tanmaygk97@gmail.com>
2019-03-27 23:28:45 +0530

    I did you MOM

--- cancer.cpp
+++ cancer.cpp
@@ -48,6 +48,7 @@ vector<pair<double,double>> graham_scan(vector<pair<double,double>> point_list){
             polar_angles.push_back(make_pair(point_list[i],polar_angler(corner_point,point_list[i])));
         printf("%lf\n",polar_angles[i].second);
     }
+    // TODO STL sort
     for(int i=0;i<point_list.size()-1;i++){
         for(int j=i+1;j<point_list.size();j++){
             if(polar_angles[i].second > polar_angles[j].second){
@@ -142,6 +143,101 @@ vector<pair<double,double>> jarvis_march(vector<pair<double,double>> point_list)
     return point_list;
 } 
 
+pair<double,double> median_of_median(vector<pair<double,double>> point_list){
+
+    if(point_list.size() <= 5){
+        for(int i = 0; i < point_list.size(); i++){
+            for(int j = i+1; j < point_list.size()-1; j++){
+                if(point_list[i].first > point_list[j].first){
+                    pair<double,double> temp = point_list[i];
+                    point_list[i] = point_list[j];
+                    point_list[j] = temp;
+                }
+            }
+        }
+        if(point_list.size()%2 == 0)
+            return make_pair((point_list[point_list.size()/2].first+point_list[point_list.size()/2-1].first)/2,(point_list[point_list.size()/2].second+point_list[point_list.size()/2-1].second)/2);
+        else
+            return point_list[point_list.size()/2];
+    }
+    int num_buckets = (point_list.size() + 4)/5; // ceil of the values
+    vector < vector < pair < double,double > > > grid;
+    
+    int bucket_index = 0;
+    int current_bucket_size = 0; 
+    vector<pair<double,double>> temp;
+
+    for(int i = 0; i < point_list.size(); i++ ){
+        
+        temp.push_back(point_list[i]);
+        current_bucket_size++;
+
+        if(current_bucket_size == 4)
+            bucket_index++;
+            current_bucket_size = 0;
+            grid.push_back(temp);
+            temp.clear();
+    }
+    // sort the buckets dirtily
+    for(int index = 0; index < num_buckets; index++){
+
+        for(int i = 0; i < grid[index].size(); i++){
+            for(int j = i+1; j < grid[index].size()-1; j++){
+        
+                if(grid[index][i].first > grid[index][j].first){
+                    pair<double,double> temp = grid[index][i];
+                    grid[index][i] = grid[index][j];
+                    grid[index][j] = temp;
+        
+                }
+            }
+        }
+    }
+
+    vector<pair<double,double>> medians;
+    for(int i = 0; i < num_buckets - 1; i++){
+        medians.push_back(grid[i][grid[i].size()/2]);
+    }
+    return median_of_median(medians);
+}
+
+pair<double,double> median(vector<pair<double,double>> point_list, int offset){
+
+// get pivot
+    pair<double,double> pivot = median_of_median(point_list);
+    vector<pair<double,double>> left,right;
+    for (int i = 0; i < point_list.size() ; i++ ){
+        if (point_list[i].first == pivot.first && point_list[i].second == pivot.second)
+            continue;
+        else if(point_list[i].first <= pivot.first){
+            left.push_back(point_list[i]);
+        }
+        else if(point_list[i].first > pivot.first){
+            right.push_back(point_list[i]);
+        }
+    }
+
+    if(left.size()  < right.size()){
+        offset -= left.size() + 1; // As we reduce our target by the number we eliminate and the pivot itself
+        return median(right,offset);
+    }
+    else if(left.size() > right.size()){
+        // offset remains the same
+        return median(left,offset);
+    }
+    else{
+        return pivot;
+    }
+
+
+// get partition
+// recurse depending on cases
+
+}
+
+
+
+
 int main(){
     fstream file;
     string line, x_coord, y_coord;
@@ -160,6 +256,8 @@ int main(){
         }
         file.close();
         vector<pair<double,double>> convex_hull = jarvis_march(point_list);
+        pair<double,double> answer = median(point_list,point_list.size());
+        printf("The median is %lf %lf\n", answer.first,answer.second);
     }
     else{
         printf("Unable to open the file\n");
