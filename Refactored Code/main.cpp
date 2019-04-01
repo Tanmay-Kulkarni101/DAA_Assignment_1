60488de20628e5df6e81b81c2b5bd4f492001917
aditya-srikanth <adityashrikanth14@gmail.com>
2019-04-01 15:53:27 +0530

    added testing

--- Refactored Code/main.cpp
+++ Refactored Code/main.cpp
@@ -1,14 +1,38 @@
 #include "data.h"
 #include "graph.h"
+#include <chrono>
+
 int main(){
+	
 	Data data; // calls default cnstr
-	data.generateTestCases(10000,0,10000);
+	data.generateTestCases(1000,0,10000);
 	Graph graph = Graph(data);
-	// graph.displayPointList();
+	
+	for (int i=0;i<3;i++){
 
-	// graph.kirk_patrick_seidel();
-	// graph.graham_scan();
-	graph.jarvis_march();
-
-    // graph.displayConvexHull();
+		Graph graph = Graph(data);
+		if(i == 0){
+			auto start = std::chrono::system_clock::now();
+			graph.kirk_patrick_seidel();
+			auto end = std::chrono::system_clock::now();
+			auto elapsed = end - start;
+			std::cout << "KPS: "<< elapsed.count() << '\n';
+		}
+		else if(i == 1){
+			auto start = std::chrono::system_clock::now();
+			graph.graham_scan();
+			auto end = std::chrono::system_clock::now();
+			auto elapsed = end - start;
+			std::cout<< "GS: " << elapsed.count() << '\n';
+		}
+		else if(i == 2){
+			auto start = std::chrono::system_clock::now();
+			graph.jarvis_march();
+			auto end = std::chrono::system_clock::now();
+			auto elapsed = end - start;
+			std::cout<< "JM: " << elapsed.count() << '\n';
+		}
+	}
+		
+	return 0;
 }
--- Refactored Code/script.sh
+++ Refactored Code/script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 g++ main.cpp graph.cpp edge.cpp data.cpp node.cpp
-time ./a.out
+./a.out
